1fe886e90988997310e5fdb5b5e7f5a0cf862827
Anton Sison <antoninakisison@gmail.com>
2020-02-06 21:51:02 +0800

    convert pages to wagtail

--- public/models.py
+++ public/models.py
@@ -3,8 +3,8 @@ from django import forms
 from django.conf import settings
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
 
 
 from modelcluster.fields import ParentalManyToManyField, ParentalKey
@@ -12,99 +12,62 @@ from modelcluster.fields import ParentalManyToManyField, ParentalKey
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
-from wagtail.admin.edit_handlers import (
-    FieldPanel,
-    MultiFieldPanel,
-    PageChooserPanel,
-    InlinePanel,
+from .blocks import (
+    HomePageBlock,
+    ProjectPageBlock,
+    ProcessPageBlock,
+    WebAppPageBlock
 )
 
 from wagtail.snippets.models import register_snippet
 from account.models import User
+from .image import SwiftkindImage
 
 
 class HomePage(Page):
     """ Home page models
     """
-    hero_title = models.CharField(max_length=525, blank=True, null=True)
-    hero_text = models.TextField(blank=True)
-    hero_cta_link = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name='Hero CTA link',
-    )
-    featured_section_1_title = models.CharField(
-        max_length=256,
-        blank=True,
-        null=True
-    )
-    feature_section_1_cover = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    featured_section_1 = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name='Featured section 1'
-    )
-
-    featured_section_2_title = models.CharField(
-        max_length=256, 
-        blank=True,
-        null=True
-    )
-    feature_section_2_cover = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    featured_section_2 = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name='Featured section 2'
+    parent_page_types = ['wagtailcore.Page']
+    body = StreamField(
+        HomePageBlock(), verbose_name="Page body", blank=True,
     )
 
     content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('hero_title'),
-            FieldPanel('hero_text'),
-            PageChooserPanel('hero_cta_link'),
-        ]),
-        MultiFieldPanel([
-            MultiFieldPanel([
-                FieldPanel('featured_section_1_title'),
-                ImageChooserPanel('feature_section_1_cover'),
-                PageChooserPanel('featured_section_1'),
-                ]),
-            MultiFieldPanel([
-                FieldPanel('featured_section_2_title'),
-                ImageChooserPanel('feature_section_2_cover'),
-                PageChooserPanel('featured_section_2'),
-                ])
-        ], heading="Featured homepage sections", classname="collapsible")
+        StreamFieldPanel('body'),
     ]
 
 
-class TeamMembersPage(Page):
-    members = ParentalManyToManyField(
-        settings.AUTH_USER_MODEL,
-        blank=True)
-    description = models.TextField(blank=True, null=True)
+class ProjectPage(Page):
+    """ Project page models
+    """
+    project = StreamField(
+        ProjectPageBlock(), verbose_name="Page projects", blank=True,
+    )
+
     content_panels = Page.content_panels + [
-        FieldPanel('description'),
-        FieldPanel('members', widget=forms.CheckboxSelectMultiple)
+        StreamFieldPanel('project'),
+    ]
+
+
+class ProcessPage(Page):
+    """ Process page models
+    """
+    body = StreamField(
+        ProcessPageBlock(), verbose_name="Page body", blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+
+class WebAppPage(Page):
+    """ Web app page
+    """
+    body = StreamField(
+        WebAppPageBlock(), verbose_name="Page body", blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
     ]
--- swiftkind/urls.py
+++ swiftkind/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('documents/', include(wagtaildocs_urls)),
     path('', include('public.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
