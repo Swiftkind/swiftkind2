de8b3adcb7ba1708b409fdabdb299da1c82b09ec
Anton Sison <antoninakisison@gmail.com>
2020-02-10 15:02:21 +0800

    update urls and main.js function

--- swiftkind/urls.py
+++ swiftkind/urls.py
@@ -4,11 +4,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('documents/', include(wagtaildocs_urls)),
+    path('cms/', include(wagtailadmin_urls)),
+    path('blog/', include('blog.urls')),
     path('', include('public.urls')),
-    path('', include('blog.urls')),
+    path('', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
